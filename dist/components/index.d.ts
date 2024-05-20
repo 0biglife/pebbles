7c29831086ffe22a03eb33d2dd566ca21b1c3eaf
0biglife <0biglife@gmail.com>
2024-05-20 22:46:50 +0900

    init

---
+++ dist/components/index.d.ts
@@ -0,0 +1,3 @@
+import './style.css';
+declare const test: () => import("react/jsx-runtime").JSX.Element;
+export default test;
---
+++ src/lib/components/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+const test = () => {
+  return <div className="style">test</div>
+}
+
+export default test;
