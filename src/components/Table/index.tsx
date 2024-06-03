238059e40f467dad054441a4fcecc3a9aec75ccd
0biglife <0biglife@gmail.com>
2024-06-03 10:54:01 +0900

    3

--- src/components/Table/index.tsx
+++
@@ -1,4 +0,0 @@
-import Table from './Table';
-import TableCell from './TableCell';
-
-export { Table, TableCell };
--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-import Button from './Button';
-import ContextMenu from './ContextMenu';
-import InputField from './InputField';
-import Typography from './Typography/index';
-import Spinner from './Spinner';
-
-export { Button, ContextMenu, InputField, Typography, Spinner };
